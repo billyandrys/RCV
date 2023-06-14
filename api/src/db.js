d06e59f0304dc127612cde6a73c49630e1d8149d
carlos carabajal <carjuancara@gmail.com>
2023-06-14 13:13:31 -0300

    ADD/model manager an vaccination

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const babyModel=require('./models/Baby')
+const babyModel = require('./models/Baby')
+const managerModel = require('./models/Manager')
+const vaccinationModel = require('./models/Vaccination')
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
 } = process.env;
@@ -12,15 +14,16 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 babyModel(sequelize)
-
+managerModel(sequelize)
+vaccinationModel(sequelize)
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Baby, Manager } = sequelize.models;
+const { Baby, Manager, Vaccination } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-//Baby.belongsToMany(Manager, { through: "Baby_Manager" })
-//Manager.belongsToMany(Baby, { through: "Baby_Manager" })
+Baby.belongsToMany(Manager, { through: "Baby_Manager" })
+Manager.belongsToMany(Baby, { through: "Baby_Manager" })
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
