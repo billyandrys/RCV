da8588e488357be4243aa6a321f5809eb54b6014
carlos carabajal <carjuancara@gmail.com>
2023-06-14 22:32:13 -0300

    UPDATE/ manager

--- api/src/routes/Index.js
+++ api/src/routes/Index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const babyRouter = require('./BabyRouter')
 /* const epsRouter = require('./EpsRouter') */
 /* const ipsRouter = require('./IpsRouter') */
-/* const responsableRouter = require('./ResponsableRouter') */
+const managerRouter = require('./ManagerRouter')
 /* const vacunacionRouter = require('./VacunacionRouter') */
 /* const usuariosRouter = require('./UsuariosRouter')*/
 
@@ -18,7 +18,7 @@ router.use(morgan('dev'))
 router.use('/baby', babyRouter);
 /* router.use('/eps', epsRouter); */
 /* router.use('/ips', ipsRouter); */
-/* router.use('/manager', managerRouter); */
+router.use('/manager', managerRouter);
 /* router.use('/vaccination', vaccinationRouter); */
 /* router.use('/users', userssRouter) */
 module.exports = router;
